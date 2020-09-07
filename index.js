12ec482be0689b582e3fe90af083c4f5843ed8d5
buchacha <egor322129@gmail.com>
2020-09-07 11:31:25 +0000

    my first commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http')
 const port = 3000
 const requestHandler = (request, response) => {
     console.log(request.url)
-    response.end('Hello Node.js Server!')
+    response.end('Hello Node.js Server with git repository!')
 }
 const server = http.createServer(requestHandler)
 server.listen(port, (err) => {
